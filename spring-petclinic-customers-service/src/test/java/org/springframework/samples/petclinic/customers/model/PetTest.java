fd3d6caba7e394c2df60aec3a46cda59d81bba95
Alex <alex@clarusway.com>
2020-11-10 16:10:22 +0000

    added 3 UTs for customer-service :q
    lr
    clr
    clear

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
@@ -1,11 +1,11 @@
 package org.springframework.samples.petclinic.customers.model;
-​
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-​
+
 import org.junit.jupiter.api.Test;
-​
+
 public class PetTest {
-​
+
     @Test
     public void testGetName(){
         //Arrange
@@ -15,7 +15,7 @@ public class PetTest {
         //Assert
         assertEquals("Fluffy", pet.getName());
     }
-​
+
     @Test
     public void testGetOwner(){
         //Arrange
